65a9a55dd3429415515dcd8fcf9a4d42af8749e1
Adeen Shukla <adeen@metafic.co>
2021-10-26 22:38:30 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,3 @@
+import PasswordInput from "./src/PasswordInput";
+
+export default PasswordInput;
---
+++ src/PasswordInput.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { TextInput, View } from "react-native";
+
+const PasswordInput = () => {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  );
+};
+
+export default PasswordInput;
